854ce9897ae5cc61f5c87d667fe5a06d753fd44b
PeeCee <59049022+passionate510@users.noreply.github.com>
2021-11-09 07:27:17 +0100

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$input = file_get_contents('php://input');
-$update = json_decode($input);
-$message = $update->$message;
-$chat_id = 2062813867;
-$token = '2075414781:AAEhP9lGPjdSjkE9YloAbBhaf4pmgotQ0Ik';
-file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=hi");
-
-?>
