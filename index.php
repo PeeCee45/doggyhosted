795a3d69357bbf705de10b9e14cda6e0a25cded1
PeeCee <59049022+passionate510@users.noreply.github.com>
2021-11-09 07:05:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $input = file_get_contents('php://input');
 $update = json_decode($input);
 $message = $update->$message;
-$chat_id = $message->$chat->id;
+$chat_id = 2062813867;
 $token = '2075414781:AAEhP9lGPjdSjkE9YloAbBhaf4pmgotQ0Ik';
 file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=hi");
 
